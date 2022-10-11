0f948c20c5d0eba2700d6a7fd913cd3a5d27f52b
Kapkaev Andrey <kapkaew@yandex.ru>
2022-10-11 23:53:00 +0300

    Add Lab to training Python skills)

---
+++ Lab_backend_0/tests/tests.py
@@ -0,0 +1,53 @@
+from unittest import TestCase
+
+import fixtures
+try:
+    from Lab_backend_0.main import Solution
+except ImportError:
+    raise AssertionError('Отсутствует класс Solution')
+
+
+solution = Solution()
+
+
+class TestSolution(TestCase):
+    def test_all_methods_allowed(self):
+        for attrib in fixtures.ATTRIBUTES:
+            try:
+                attrib = getattr(solution, attrib)
+            except AttributeError:
+                raise AssertionError(
+                    f'Атрибут {attrib} отсутствует у класса Solution'
+                )
+
+    def test_sum_number_in_string(self):
+        for key, value in fixtures.SUM_STRING_NUMBER.items():
+            self.assertEqual(
+                solution.sum_number_in_string(key), value,
+                'Ваш скрипт работает неверно проверьте всё ещё раз)'
+            )
+
+    def test_leader_string(self):
+        for key, value in fixtures.LEADERS.items():
+            self.assertEqual(
+                solution.get_leader_string(key), value,
+                'Если вас постигла неудача просто улыбнитесь этому сообщению'
+            )
+
+    def test_to_json(self):
+        @solution.to_json
+        def test_func(data: dict):
+            return data
+
+        for string_json, value in fixtures.json_datas.items():
+            self.assertEqual(
+                test_func(value), string_json,
+                'Убедитесь что ваш декоратор работает правильно.'
+            )
+
+    def test_get_spiral_numbers(self):
+        for key, value in fixtures.SPIRAL_NUMBERS.items():
+            matrix = solution.get_spiral_number_matrix(key)
+            self.assertIsInstance(matrix, list, 'Метод должен возвращать список')
+            result_string = '\n'.join([' '.join(list(map(str, line))) for line in matrix])
+            self.assertEqual(result_string, value)
