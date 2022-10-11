010eb8c2e1aedc6db2984b9f6f592ad49054f99a
Kapkaev Andrey <kapkaew@yandex.ru>
2022-10-12 00:09:23 +0300

    Add Lab to training Python skills)

---
+++ Lab_backend_0/main.py
@@ -0,0 +1,19 @@
+import json
+
+
+class Solution:
+    def sum_number_in_string(self, number_string: str) -> int:
+        pass
+
+    def get_leader_string(self, steps: int) -> str:
+        pass
+
+    def to_json(self, func):
+        pass
+
+    def get_spiral_number_matrix(self, number):
+        pass
+
+
+if __name__ == '__main__':
+    solution = Solution()
