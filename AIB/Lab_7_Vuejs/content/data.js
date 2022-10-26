a15e699b7124930cd4d8fe9fbe73e383b5f3158e
kocten <kocten-software@ya.ru>
2022-10-26 13:17:13 +0300

    AIB Lab7 & Lab 8 created

---
+++ AIB/Lab_7_Vuejs/content/data.js
@@ -0,0 +1,38 @@
+let data = [
+	{
+		"title" : "Работа с Git в Visual Studio Code",
+		"intro" : "Один из способов работать с Гитом.",
+		"labels" : ["Git", "Консоль"],
+		"image" : "https://github.com/RSTU-Citg-Space/web_lab/blob/frontend/AVB/Lab_7_Vuejs/content/post-1.png",
+	},
+	{
+		"title" : "Adobe покупают Фигму. Всё пропало?",
+		"intro" : "Не факт, но есть разные мнения.",
+		"labels" : ["Figma"],
+		"image" : "https://github.com/RSTU-Citg-Space/web_lab/blob/frontend/AVB/Lab_7_Vuejs/content/post-2.png",
+	},
+	{
+		"title" : "Все научились программировать. А дальше-то что?",
+		"intro" : "Ученье — свет.",
+		"labels" : ["Обучение", "Карьера"],
+		"image" : "https://github.com/RSTU-Citg-Space/web_lab/blob/frontend/AVB/Lab_7_Vuejs/content/post-3.png",
+	},
+	{
+		"title" : "Мне тяжело учиться, кто поможет?",
+		"intro" : "Попали в патовую ситуацию? Тогда кликайте на статью.",
+		"labels" : ["Обучение"],
+		"image" : "https://github.com/RSTU-Citg-Space/web_lab/blob/frontend/AVB/Lab_7_Vuejs/content/post-4.png",
+	},
+	{
+		"title" : "Ключевые навыки фронтендеров в 2022",
+		"intro" : "Ежегодное исследование рынка от HTML Academy.",
+		"labels" : ["Обучение", "Карьера"],
+		"image" : "https://github.com/RSTU-Citg-Space/web_lab/blob/frontend/AVB/Lab_7_Vuejs/content/post-5.png",
+	},
+	{
+		"title" : "Фронтенд-новости",
+		"intro" : "WAI-ARIA, новинки HTML-форм и 12 ошибок любого джаваскриптера.",
+		"labels" : ["Новости"],
+		"image" : "https://github.com/RSTU-Citg-Space/web_lab/blob/frontend/AVB/Lab_7_Vuejs/content/post-6.png",
+	},
+]
